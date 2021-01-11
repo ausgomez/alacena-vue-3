f30022ebe425e318e203aed52d8b07d3b814b1d7
Aus Gomez <austrebertog@gmail.com>
2021-01-10 22:31:47 -0600

    adding final build

--- src/views/builder.vue
+++ src/views/builder.vue
@@ -1,38 +1,82 @@
 <template>
-  <div class="flex container mx-auto mt-5 space-x-4" style="min-height: 400px;">
-    <div class="w-1/3 shadow-lg p-4 rounded-xl bg-white">
-      <h1>Search Items</h1>
+  <div class="flex flex-col m-3 space-y-3">
+    <div class="p-4 rounded-lg  space-y-4" style="background-color: #373737; min-height: 13rem;">
+      <h1 class="text-2xl font-bold text-white text-center">Search for Ingredients</h1>
       <input
-        class="w-full h-12 text-2xl px-4 rounded-xl bg-gray-100 text-gray-500 shadow"
         type="text"
-        placeholder="Input"
+        class="p-6 rounded-lg h-8 text-gray-200 placeholder-gray-500 w-full"
+        placeholder="&#x1F50D; Search for ingredients"
         v-model="ingredientQuery"
         @keyup.enter="searchUp"
       />
-      <div class="mt-4">
-        <ul>
-          <li v-for="(item, i) in ingredients" :key="i" @click="addToBasket(item)">
-            <div class="h-24 bg-blue-300 mt-3 rounded-xl shadow hover:shadow-md">Ingredient {{ item }} {{ i }}</div>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="w-1/3 shadow-lg rounded-xl bg-white p-4">
-      <h1>Basket Items</h1>
-      <button class="h-12 w-full rounded-xl bg-green-300 shadow" @click="findRecipes">Find Recipes</button>
-      <ul class="flex flex-wrap mt-5">
-        <li v-for="(item, i) in basket" :key="i" @click="removeFromBasket(item)" class="m-5">
-          <img :src="`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`" />
-          <!-- <div class="h-24 bg-blue-300 mt-3 rounded-xl shadow hover:shadow-md">{{ item.image }}</div> -->
+      <ul class="flex overflow-x-auto space-x-2 text-center text-white uppercase">
+        <li v-for="(item, i) in ingredients" :key="i" @click="addToBasket(item)">
+          <!--  -->
+          <div class="h-24 w-24 flex">
+            <!--  -->
+            <div
+              class="w-full flex justify-center items-center bg-black rounded-3xl shadow-lg h-full bg-local overflow-hidden"
+              :style="
+                `background-image: url(https://spoonacular.com/cdn/ingredients_100x100/${item.image}); background-size: cover;`
+              "
+            >
+              <!--  -->
+            </div>
+            <!--  -->
+          </div>
+          <p>{{ item.name }}</p>
+          <!--  -->
         </li>
       </ul>
     </div>
-    <div class="w-1/3 shadow-lg rounded-xl bg-white p-4">
-      <h1>Recipe Items</h1>
-      <ul class="flex flex-wrap">
-        <li v-for="(item, i) in recipes" :key="i" @click="removeFromBasket(item)" class="m-2">
-          <img :src="item.image" class="rounded-full w-32 h-32" alt="" />
-          <!-- <div class="h-12 bg-blue-300 mt-3 rounded-xl shadow hover:shadow-md">Ingredient {{ item.title }} {{ i }}</div> -->
+
+    <!--  -->
+    <div class="p-4 rounded-lg space-y-4 text-white text-center" style="background-color: #373737; min-height: 13rem;">
+      <h1 class="text-2xl font-bold">Your Basket</h1>
+      <div v-if="basket.length > 0">
+        <ul class="flex overflow-x-auto space-x-2 text-center text-white uppercase">
+          <li
+            v-for="(item, i) in basket"
+            :key="i"
+            @click="removeFromBasket(item)"
+            class="flex flex-col items-center justify-center align-center"
+          >
+            <!--  -->
+            <div class="h-24 w-24 flex">
+              <!--  -->
+              <div
+                class="w-full flex justify-center items-center bg-black rounded-3xl shadow-lg h-full bg-local overflow-hidden"
+                :style="
+                  `background-image: url(https://spoonacular.com/cdn/ingredients_100x100/${item.image}); background-size: cover;`
+                "
+              >
+                <!--  -->
+              </div>
+              <!--  -->
+            </div>
+            <p class="text-center">{{ item.name }}</p>
+            <!--  -->
+          </li>
+        </ul>
+        <button @click="findRecipes" class="p-4 bg-yellow-500 text-white rounded mt-6">
+          Explore Recipes
+        </button>
+      </div>
+
+      <div v-else class="h-20 flex items-center justify-center">
+        <p class="w-3/4 text-center">Your Basket is Empty, pick items from the top.</p>
+      </div>
+    </div>
+
+    <div
+      v-if="recipes.length > 0"
+      class="p-4 rounded-lg  space-y-4 text-white text-center"
+      style="background-color: #373737; min-height: 13rem;"
+    >
+      <h1 class="text-2xl font-bold">Recipes found</h1>
+      <ul v-if="!loading" class="text-white overflow-x-auto flex">
+        <li v-for="(recipe, i) in recipes" :key="i">
+          <Recipe :recipe="recipe" />
         </li>
       </ul>
     </div>
@@ -42,9 +86,13 @@
 <script>
 import axios from "axios"
 import { ref } from "vue"
+import Recipe from "@/components/Recipe"
 
 export default {
   name: "Builder",
+  components: {
+    Recipe,
+  },
   setup() {
     const ingredients = ref([])
     const basket = ref([])
